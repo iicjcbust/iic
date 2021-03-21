97cd838ce918857a94507ae8c61bb7a33fc5b2bc
Avinash <avinashsingh880086@gmail.com>
2021-03-21 17:06:58 +0530

    Delete IICProject directory

--- IICProject/home/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Events,announcement,organizer,Contact,JoinIIC
-# Register your models here.
-admin.site.register(Events)
-admin.site.register(announcement)
-admin.site.register(organizer)
-admin.site.register(Contact)
-admin.site.register(JoinIIC)
